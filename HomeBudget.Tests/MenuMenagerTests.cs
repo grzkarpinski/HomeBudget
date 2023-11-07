1ca2f27ea5ccad3ef1979af12970e2e1d409bb70
grzkarpinski <66385734+grzkarpinski@users.noreply.github.com>
2023-11-07 16:16:23 +0100

    add Tests project add moq libary add fluentAssertions libary

---
+++ HomeBudget.Tests/MenuMenagerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBudget.Tests
+{
+    public class MenuMenagerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
